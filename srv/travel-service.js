36b8c089f3f22dafc1dde4d9a9a046bc9dcad135
Jhony Gautam <lakshita.flow@gmail.com>
2026-09-05 06:13:10 +0000

    07. ii). Configuring Micro Charts to Visualize Key Figures

--- srv/travel-service.js
+++ srv/travel-service.js
@@ -78,8 +78,8 @@ class TravelService extends cds.ApplicationService {
 
 
     /**
-     * Update the Travel's TotalPrice when a Supplement's Price is modified.
-     */
+   * Update the Travel's TotalPrice when a Supplement's Price is modified.
+   */
     this.after('UPDATE', 'BookingSupplement.drafts', async (_, req) => {
       if ('Price' in req.data) {
         // We need to fetch the Travel's UUID for the given Supplement target
@@ -87,9 +87,19 @@ class TravelService extends cds.ApplicationService {
           .from(BookingSupplement.drafts).where({ BookSupplUUID: req.data.BookSupplUUID })
         const { travel } = await SELECT.one`to_Travel_TravelUUID as travel`.from(Booking.drafts)
           .where`BookingUUID = ${booking} `
+        await this._update_totals_supplement(booking)
         return this._update_totals4(travel)
       }
     })
+
+    /**
+  * Update the Booking's TotalSupplPrice
+  */
+    this._update_totals_supplement = async function (booking) {
+      const { totals } = await SELECT.one`coalesce (sum (Price),0) as totals`.from(BookingSupplement.drafts).where
+        `to_Booking_BookingUUID = ${booking}`
+      return UPDATE(Booking.drafts, booking).with({ TotalSupplPrice: totals })
+    }
 
     /**
      * Update the Travel's TotalPrice when a Booking Supplement is deleted.
